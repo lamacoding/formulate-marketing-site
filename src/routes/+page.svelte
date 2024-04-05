4ef37318c06561fc345c7fce1761d15e9a888063
Matthias Langwieser <37292244+lamacoding@users.noreply.github.com>
2024-04-05 15:56:52 +0200

    Add detail cards to start page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import {P, Heading, Li, List, Img, Button} from "flowbite-svelte";
+  import {P, Heading, Li, List, Img, Button, Card} from "flowbite-svelte";
   import {CheckCircleSolid} from "flowbite-svelte-icons";
 </script>
 
@@ -44,5 +44,27 @@
       </div>
     </div>
     <Img src="undraw_forms_re_pkrt.svg" alt="form" class="lg:w-1/2 m-auto"/>
+  </div>
+
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-20 mx-5 mt-14 md:mt-40">
+    <Card>
+      <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Freiheit</h5>
+      <p class="mb-5 font-normal text-gray-700 dark:text-gray-400 leading-tight">Mit unserer Open-Source-Plattform sind
+        Sie nicht durch restriktive Lizenzen oder teure Abonnements eingeschränkt. Erstellen Sie Formulare, die Ihre
+        einzigartige Vision widerspiegeln, ohne dabei Ihr Budget zu sprengen.</p>
+    </Card>
+    <Card>
+      <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Anpassung ohne Kompromisse</h5>
+      <p class="mb-5 font-normal text-gray-700 dark:text-gray-400 leading-tight">Passen Sie Ihre Formulare an Ihre
+        spezifischen Anforderungen an mit unserer flexiblen und anpassbaren Plattform. Von Designelementen bis hin zur
+        Funktionalität - übernehmen Sie die Kontrolle über jeden Aspekt Ihrer Formulare und heben Sie das Nutzererlebnis
+        auf ein neues Level.</p>
+    </Card>
+    <Card>
+      <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+        acquisitions 2021</h5>
+      <p class="mb-5 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise
+        technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </Card>
   </div>
 </div>
